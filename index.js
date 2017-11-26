19d1b575e729cd5e27e2ba470511de52dcc9fb5f
Konstantin Gusev <guvkon@gmail.com>
2017-11-26 20:59:46 +0700

    Finish crawler

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const Crawler = require('node-webcrawler');
-const url = require('url');
+let indexCrawler = require('./lib/index_crawler');
 
-let c = new Crawler({
-    maxConnections: 10,
-    callback: (error, result, $) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log($("title").text());
-        }
+const baseUrl = 'http://rpgmapshare.com/piwigo/gallery/index.php?/category/58';
+const lastPage = 36;
+const itemsPerPage = 15;
+const pagerSuffixUrl = '/start-';
+
+for (let i = 0; i < lastPage; i++) {
+    let url = baseUrl;
+    if (0 !== i) {
+        url += pagerSuffixUrl;
+        url += i * itemsPerPage;
     }
-});
-
-c.queue('http://www.amazon.com');
-c.queue(['http://www.google.com/', 'http://www.yahoo.com']);
+    indexCrawler.queue(url);
+}
